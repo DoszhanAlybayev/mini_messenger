be23b38041b6b907d9917861b9368ece6640cb3d
Doszhan <doszhan@MacBook-Air-Doszhan.local>
2025-07-20 16:52:33 +0500

    создал оболочку проекта

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_messenger/pages/login_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main() {
+  runApp(const MiniMessengerApp());
+}
+
+class MiniMessengerApp extends StatelessWidget {
+  const MiniMessengerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mini Messenger',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
 }
